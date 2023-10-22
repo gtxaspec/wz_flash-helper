41bf433adb85918ff3780d7770d023820be8986b
archandanime <36922812+archandanime@users.noreply.github.com>
2023-10-22 22:59:35 +0700

    Add led blinking for switch firmware operation

---
+++ initramfs_skeleton/blink_red_and_blue_leds.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+red_led_gpio="38"
+blue_led_gpio="39"
+
+blink_red_led() {
+	echo 0 > /sys/class/gpio/gpio$red_led_gpio/value
+	sleep 1
+	echo 1 > /sys/class/gpio/gpio$red_led_gpio/value
+	sleep 1
+}
+
+blink_blue_led() {
+	echo 0 > /sys/class/gpio/gpio$blue_led_gpio/value
+	sleep 1
+	echo 1 > /sys/class/gpio/gpio$blue_led_gpio/value
+	sleep 1
+}
+
+while true; do
+	blink_red_led
+	blink_blue_led
+done
+

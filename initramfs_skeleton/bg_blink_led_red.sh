46a98e4885fdc9d47a45d092f2f1fc081c6d4559
archandanime <36922812+archandanime@users.noreply.github.com>
2023-10-25 01:55:22 +0700

    More edits

--- initramfs_skeleton/bg_blink_led_red.sh
+++ initramfs_skeleton/bg_blink_led_red.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-red_led_gpio="38"
-while true; do
-	echo 0 > /sys/class/gpio/gpio$red_led_gpio/value
-	sleep 1
-	echo 1 > /sys/class/gpio/gpio$red_led_gpio/value
-	sleep 1
-done
+blink_led_red() {
+	local red_led_gpio="38"
+	while true; do
+		echo 0 > /sys/class/gpio/gpio$red_led_gpio/value
+		sleep 1
+		echo 1 > /sys/class/gpio/gpio$red_led_gpio/value
+		sleep 1
+	done
+}
+
+blink_led_red

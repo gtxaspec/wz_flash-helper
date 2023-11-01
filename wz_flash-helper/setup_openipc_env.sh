fb9a6ca9839eaef27d3439d910f5e37fd68966a2
archandanime <36922812+archandanime@users.noreply.github.com>
2023-11-01 15:36:17 +0700

    Update OpenIPC env import script

--- wz_flash-helper/setup_openipc_env.sh
+++ wz_flash-helper/setup_openipc_env.sh
@@ -3,8 +3,27 @@
 # Description: Write env variables from file to env partition
 #
 
+
+WIFI_SSID="Wi-Fi name"
+WIFI_PASSWORD="Wi-Fi password"
+
+
+
+## These variables are optional, you can set them later
+# MAC address example: 11:22:33:44:55:66
+MAC_ADDRESS=""
+# Timezone example: America/Los_Angeles
+TIMEZONE=""
+
+
+
+
+
+	
+	
+	
 function get_wifi_gpio_pin() {
-# Description: Return GPIO pin for a model
+# Description: Return GPIO pin for queried camera model
 # Syntax: get_wifi_gpio_pin <model>
 	local model="$1"
 	case $model in
@@ -19,31 +38,58 @@ function get_wifi_gpio_pin() {
 	esac
 }
 
-function initialize_gpio_wifi() {
-# Description: Initialize wifi by setting gpio pin
-# Syntax: get_wifi_gpio_pin <gpio_pin>
+function get_wifi_device_id() {
+# Description: Initialize wifi board by setting its gpio pin then get it device id
+# Syntax: get_wifi_id <gpio_pin>
 	local gpio_pin="$1"
 	
 	echo $gpio_pin > /sys/class/gpio/export 
 	echo out > /sys/class/gpio/$gpio_pin/direction 
 	echo 1 > /sys/class/gpio/$gpio_pin/value 
-	
 	echo INSERT > /sys/devices/platform/jzmmc_v1.2.1/present
-	cat /sys/bus/mmc/devices/mmc1\:0001/mmc1\:0001\:1/vendor
+	
+	local wifi_device_id=$(cat /sys/bus/mmc/devices/mmc1\:0001/mmc1\:0001\:1/vendor)
+	echo -n $wifi_device_id
 }
 
 
-function main() {
-	openipc_env_file="/sdcard/wz_flash-helper/openipc_env.txt"
-	
-	case $chip_family in
-		"t20")
-			config_partname="stock_para"
+function detect_wifi_driver() {
+	case $model in
+		"pan_v1")
+			wifi_driver="rtl8189ftv-t20-wyze-pan-v1"
+			;;
+		"v2")
+			wifi_driver="rtl8189ftv-t20-wyze-v2"
+			;;
+		"v3")
+			local wifi_gpio_pin=$(get_wifi_gpio_pin $model)
+			local wifi_device_id=$(get_wifi_device_id $wifi_gpio_pin)
+			
+			[[ "$wifi_device_id" == "0x024c" ]] && wifi_driver="rtl8189ftv-t31-wyze-v3"
+			[[ "$wifi_device_id" == "0x007a" ]] && wifi_driver="atbm603x-t31-wyze-v3"
+			;;
+			
+		"v3c")
+			wifi_driver="atbm603x-t31-wyze-v3"
+			;;
+		"pan_v2")
+			wifi_driver="atbm603x-t31-wyze-pan-v2"
+			;;
+	esac
+}
 
-		"t31")
-			config_partname="stock_cfg"
+function set_openipc_custom_env() {	
+	fw_setenv -l /tmp -c /etc/fw_env.config wlandev $wifi_driver
+	fw_setenv -l /tmp -c /etc/fw_env.config wlanssid $WIFI_SSID
+	fw_setenv -l /tmp -c /etc/fw_env.config wlanpass $WIFI_PASSWORD
+	
+	[[ ! "$MAC_ADDRESS" == "" ]] && fw_setenv -l /tmp -c /etc/fw_env.config wlanaddr $MAC_ADDRESS
+	[[ ! "$TIMEZONE" == "" ]] && fw_setenv -l /tmp -c /etc/fw_env.config timezone $TIMEZONE	
+}
+
+function main() {
+	detect_wifi_driver
+	set_openipc_custom_env
 }
 
 main
-for 
-	fw_setenv -l /tmp -c /sdcard/fw_env.config

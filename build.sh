2770ce1991aa31f865b803e5b37133ad9cbb928d
archandanime <36922812+archandanime@users.noreply.github.com>
2023-11-04 23:50:12 +0700

    Add build script and kernel patch files

---
+++ build.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+#
+#
+#
+
+action="${1}"
+SoC="${2}"
+
+
+
+
+function show_syntax() {
+	echo "./build.sh <action> [SoC]"
+	echo "Actions: patch initramfs kernel release clean"
+}
+
+function get_sdcard_kernel_filename() {
+	case ${SoC} in
+		"t20")
+			echo -n "factory_ZMC6tiIDQN"
+			;;
+		"t31")
+			echo -n "factory_t31_ZMC6tiIDQN"
+			;;
+	esac
+}
+
+function make_initramfs() {
+	mkdir -p initramfs_root/{dev,proc,sys,tmp}
+	cp -r initramfs_root /tmp
+
+	( cd /tmp/initramfs_root && find . | fakeroot cpio --create --format='newc' | gzip > /tmp/initramfs.cpio )
+}
+
+function patch_kernel_config() {
+	patch firmware/br-ext-chip-ingenic/board/t21/kernel/t20.generic.config < kernel/kernel.patch.t20
+	patch firmware/br-ext-chip-ingenic/board/t31/kernel/t31.generic.config < kernel/kernel.patch.t31
+}
+
+function compile_kernel() {
+	[ ! -f /tmp/initramfs.cpio ] && make_initramfs
+	( cd firmware && BOARD=${SoC}_ultimate_defconfig make br-linux )
+	
+	mkdir output/${SoC}
+	cp firmware/output/images/uImage output/${SoC}/${sdcard_kernel_filename}
+}
+
+function make_release() {
+	mkdir -p output/${SoC}
+	[ ! -f output/${SoC}/${sdcard_kernel_filename} ] && compile_kernel
+
+	cp -r wz_flash-helper output/${SoC}
+	mv output/${SoC}/wz_flash-helper/restore/stock_${SoC}.conf output/${SoC}/wz_flash-helper/restore/stock.conf
+	rm output/${SoC}/wz_flash-helper/restore/stock_*.conf
+
+	( cd output/${SoC} && zip -r ${version}_${SoC}.zip . )
+	rm -r output/${SoC}/wz_flash-helper
+}
+
+function clean_up() {
+	rm -r output/*
+	rm -r initramfs_root/{dev,proc,sys,tmp}
+	rm /tmp/initramfs.cpio
+}
+
+
+sdcard_kernel_filename=$(get_sdcard_kernel_filename ${SoC})
+version=$(cat initramfs_root/prog_ver.txt)
+
+mkdir -p output
+
+case ${action} in
+	"patch")
+		patch_kernel_config
+		;;
+	"initramfs")
+		make_initramfs
+		;;
+	"kernel")
+		compile_kernel
+		;;
+	"release")
+		make_release
+		;;
+	"clean")
+		clean_up 2>/dev/null
+		;;
+	*)
+		show_syntax
+		;;
+esac
+
--- initramfs_root/action_execute_custom_script.sh
+++ initramfs_root/action_execute_custom_script.sh
@@ -6,14 +6,14 @@
 function execute_custom_script() {
 	msg "custom_script value is set to $custom_script"
 	
-	[ ! -f /sdcard/$prog_dir/scripts/$custom_script ] && { msg "Custom script file is missing" ; return 1 ; }
+	[ ! -f $prog_dir/scripts/$custom_script ] && { msg "Custom script file is missing" ; return 1 ; }
 	[[ "$dry_run" == "yes" ]] && { msg "Custom script does not run when dry run is active" ; return 1 ; }
 
 	msg
 	msg "---------- Begin custom script ----------"
 	msg "Running script: $(basename $custom_script)"
 	msg
-	source /sdcard/$prog_dir/scripts/$custom_script || return 1
+	source $prog_dir/scripts/$custom_script || return 1
 	msg "----------- End custom script -----------"
 	msg
 }

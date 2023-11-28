9745c7b2024e439972d5716a38565113965beb5f
archandanime <36922812+archandanime@users.noreply.github.com>
2023-11-29 01:47:49 +0700

    Add unpad_partimg function

--- initramfs_root/functions_misc.sh
+++ initramfs_root/functions_misc.sh
@@ -8,6 +8,38 @@ function gen_4digit_id() {
 	shuf -i 1000-9999 -n 1
 }
 
+unpad_partimg() {
+# Create a partition image with padded blocks removed from a partition image
+# Syntax: unpad_partimg <partimg> <blocksize>
+	local partimg="$1"
+	local blocksize="$2"
+	local blocksize=$(( $blocksize * 1024 ))
+	
+	[ ! -f $partimg ] && { echo "Partition image $partimg is missing" ; return 1 ; }
+	
+	local partimg_size=$(du -b $partimg | cut -f -1)
+	local padding_block=$(dd if=/dev/zero bs=$blocksize count=1 status=none | tr '\0' '\377')
+	local partimg_totalblocks=$(( $partimg_size/$blocksize ))
+	local partimg_totalblocks_minusone=$(( $partimg_totalblocks - 1 ))
+
+	local block_contents=""
+	local padding_blocks_total="0"
+	for block in $(seq $partimg_totalblocks_minusone -1 0); do
+		block_contents=$(dd if=$partimg bs=$blocksize count=1 skip=$block status=none | tr -d '\000')
+		if [[ "$block_contents" == "$padding_block" ]]; then
+			padding_blocks_total=$(( $padding_blocks_total + 1 ))
+			echo "Block $block is padded"
+		else
+			break
+		fi
+	done
+
+	echo "Number of data blocks: $(( $partimg_totalblocks - $padding_blocks_total ))"
+	echo "Number of padded blocks: $padding_blocks_total"
+	dd if=$partimg of=$partimg.unpadded bs=$blocksize count=$(( $partimg_totalblocks - $padding_blocks_total )) status=none
+	echo "Output file: $partimg.unpadded"
+}
+
 function rollback_boot_partition() {
 # Description: Check if written boot partition is valid. If not, rollback with the backup boot image
 	msg

789a3ae3ffa6ee0f4e459599986591e179ae2cc4
archandanime <36922812+archandanime@users.noreply.github.com>
2023-12-20 15:24:24 +0700

    Fix SD card is not mounted by /sbin scripts

--- initramfs_root/sbin/mount_sdcard.sh
+++ initramfs_root/sbin/mount_sdcard.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo -n "Mounting SD card... "
-! mountpoint -q /sdcard || mount -t vfat /dev/mmcblk0p1 /sdcard -o rw,umask=0000,dmask=0000 && echo "done" || echo "failed"
+mountpoint -q /sdcard || mount -t vfat /dev/mmcblk0p1 /sdcard -o rw,umask=0000,dmask=0000 && echo "done" || echo "failed"
 echo
--- initramfs_root/sbin/wz_edit_config.sh
+++ initramfs_root/sbin/wz_edit_config.sh
@@ -2,6 +2,6 @@
 
 source /variables_prog_info.sh
 
-! mountpoint -q /sdcard || mount -t vfat /dev/mmcblk0p1 /sdcard -o rw,umask=0000,dmask=0000
+mountpoint -q /sdcard || mount -t vfat /dev/mmcblk0p1 /sdcard -o rw,umask=0000,dmask=0000
 nano $prog_config_file
 sync

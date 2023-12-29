b3cc984f9a6e1c7007873af1c369cbdcad6e5770
archandanime <36922812+archandanime@users.noreply.github.com>
2023-12-29 21:58:32 +0700

    Update 01_audio.sh

--- initramfs_overlay/t31/kernel_module.d/01_audio.sh
+++ initramfs_overlay/t31/kernel_module.d/01_audio.sh
@@ -3,7 +3,7 @@
 # Description: Load audio driver
 #
 
-if fw_printenv wlandev | grep -q "atbm603x-t31-wyze-pan-v2" || grep -q "recovery_wcpv2.bin" /boot_backup.img.strings ; then
+if fw_printenv devicemodel | grep -q "pan_v2" || grep -q "recovery_wcpv2.bin" /boot_backup.img.strings ; then
 	insmod /kernel_module.d/modules/audio.ko spk_gpio=7
 else
 	insmod /kernel_module.d/modules/audio.ko

7042d47b491a1f045a1e209f46ccd0ea2648690e
archandanime <36922812+archandanime@users.noreply.github.com>
2024-01-02 21:18:18 +0700

    Allow booting a custom SD card kernel on the next boot after switching profile

--- initramfs_overlay/t31/profile.d/stock/np_variables.sh
+++ initramfs_overlay/t31/profile.d/stock/np_variables.sh
@@ -9,3 +9,6 @@ np_all_partname_list="boot kernel rootfs app kback aback cfg para"
 ## Where all partition images will be saved
 ## Same as cp_backup_path
 np_images_path="/sdcard/wz_flash-helper/restore/stock"
+
+## Name of SD card kernel
+np_sdcard_kernel_name="factory_t31_ZMC6tiIDQN"
--- initramfs_root/action_copy_new_sdcard_kernel.sh
+++ initramfs_root/action_copy_new_sdcard_kernel.sh
@@ -5,17 +5,25 @@
 
 function copy_new_sdcard_kernel() {
 	[ ! -f /sdcard/$new_sdcard_kernel ] && { msg_color lightbrown "File /sdcard/$new_sdcard_kernel is missing, skipping" ; return 0 ; }
-	[[ "$switch_profile" == "yes" ]] && { msg_color lightbrown "New SD card kernel will not be copied when switch_profile is enabled, skipping" ; return 0 ; }
 	[[ "$dry_run" == "yes" ]] && { msg_color lightbrown "New SD card kernel is not copied when dry run is active, skipping" ; return 0 ; }
+
+	[[ "$switch_profile" == "yes" ]] && msg_color lightbrown "Warning: switch_profile is enabled, new SD card kernel might not be compatile with the next profile"
+
+	if [[ "$switch_profile" == "yes" ]]; then
+		source /profile.d/$next_profile/np_variables.sh
+		local new_sdcard_kernel_name=$np_sdcard_kernel_name
+	else
+		local new_sdcard_kernel_name=$cp_sdcard_kernel_name
+	fi
 
 	msg
 	msg_nonewline "Copying "
 	msg_color_nonewline cyan "/sdcard/$new_sdcard_kernel "
 	msg_nonewline "to "
-	msg_color_nonewline cyan "/sdcard/$cp_sdcard_kernel_name"
+	msg_color_nonewline cyan "/sdcard/$new_sdcard_kernel_name"
 	msg_nonewline "... "
 
-	cp /sdcard/$new_sdcard_kernel /sdcard/$cp_sdcard_kernel_name && msg_color green "done" || { msg_color red "failed" ; return 1 ; }
+	cp /sdcard/$new_sdcard_kernel /sdcard/$new_sdcard_kernel_name && msg_color green "done" || { msg_color red "failed" ; return 1 ; }
 }
 
 copy_new_sdcard_kernel || return 1
